6236de566f871cb2e651f57991083759998c9e3e
Angie <angielisata@gmail.com>
2021-04-20 17:03:25 -0700

    added option3.js

---
+++ option3.js
@@ -0,0 +1,44 @@
+var async = require('async'); // https://www.npmjs.com/package/async
+const ID_LENGTH = 27;
+
+async function loadIds(id){
+    var request = new XMLHttpRequest()
+    var url = ' https://eluv.io/items/'
+    var authHeader = btoa(id)
+
+    request.open('GET', url+id, true)
+    request.setRequestHeader('Authorization', String(id) );
+    request.onload = function() {
+
+        if(this.readyState === 4 && this.status === 200){// if success
+            var data = JSON.parse(request.responseText)// access data here
+            console.log(data)
+        }
+        else if(request.status === 404){// if error
+            console.log("Error, 404 Not Found")
+        }
+    }
+    request.onerror = function(){
+      console.log("Error looking up",id)
+    }
+    request.send()
+}
+
+function printResults(err, results){
+  console.log(results) // all unique id information
+}
+
+function getIds(ids_to_retrieve){
+  var idList = ids_to_retrieve
+
+  //parse valid ids
+  idList = new Set(idList) // remove duplicates
+  idList =  idList.filter((id)=> {
+    return length(id) === ID_LENGTH
+  });
+
+  // send 5 simultaneous requests , wait until info return
+  // send more requests until the idList is complete
+  var url = ' https://eluv.io/items/'
+  async.mapLimit(idList, 5, loadIds(id, printResults)) //5 async calls to api made each time until all ids completed
+}
